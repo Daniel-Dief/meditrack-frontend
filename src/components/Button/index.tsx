8cb00cfd27702159643a109a89b989630c3fa46b
Daniel Santos <danieldiefsantos@gmail.com>
2025-06-08 13:11:39 -0300

    feat: botão padrão

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,22 @@
+interface Props {
+    onClick?: () => void;
+    children?: React.ReactNode;
+    className?: string;
+    color?: 'primary' | 'secondary';
+    size?: 'sm' | 'md' | 'lg';
+}
+
+export default function Button( { onClick, children, className, color, size } : Props ) {
+    return (
+        <button
+            onClick={onClick}
+            className={`px-4 py-2 rounded transition-colors focus:outline-none ${className}
+                ${color === 'primary' ? 'bg-gradient-to-r from-[#A8DADC] to-[#B8E6E8] text-white' : ''}
+                ${color === 'secondary' ? 'bg-white hover:bg-[#B8E6E8] text-black cursor-pointer transition duration-300' : ''}
+                ${size === 'sm' ? 'text-sm' : size === 'lg' ? 'text-lg' : 'text-base'}
+            `}
+        >
+            {children}
+        </button>
+    )
+}
