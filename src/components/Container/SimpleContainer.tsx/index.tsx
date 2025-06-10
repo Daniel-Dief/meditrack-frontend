28e8d37956172aa8c1eae9de85264c3577893128
Daniel-Dief <danieldiefsantos@gmail.com>
2025-06-09 21:06:10 -0300

    display: ajustar tamanho do container

--- src/components/Container/SimpleContainer.tsx/index.tsx
+++ src/components/Container/SimpleContainer.tsx/index.tsx
@@ -6,7 +6,7 @@ interface Props {
 
 export default function SimpleContainer({ children }: Props) {
     return (
-        <div className="w-full h-fit p-4 border border-gray-200 rounded-2xl flex flex-col items-center m-6 gap-4">
+        <div className="w-auto h-fit p-4 border border-gray-200 rounded-2xl flex flex-col items-center m-6 gap-4">
             {children}
         </div>
     )
