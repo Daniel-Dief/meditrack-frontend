1084fa563cf92d1f98b9444b0687bb111745722b
Daniel-Dief <danieldiefsantos@gmail.com>
2025-06-09 21:04:37 -0300

    feat: adicionar parametro de tamanho

--- src/components/Texts/Title.tsx
+++ src/components/Texts/Title.tsx
@@ -1,6 +1,11 @@
-export default function Title({ text }: { text: string }) {
+interface Props {
+    text: string,
+    size?: "big"
+}
+
+export default function Title({ text, size }: Props) {
     return (
-        <h1 className="text-2xl text-black font-bold">
+        <h1 className={`text-black font-bold ${size === "big" ? "text-3xl" : "text-2xl"}`}>
             {text}
         </h1>
     );
