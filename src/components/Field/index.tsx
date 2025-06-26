e61637124c0c73a0a48f2a795544f85f7dc26f48
Daniel-Dief <danieldiefsantos@gmail.com>
2025-06-26 20:24:19 -0300

    feat: atualizar field component

--- src/components/Field/index.tsx
+++ src/components/Field/index.tsx
@@ -2,15 +2,20 @@ import type { Ref, HTMLInputTypeAttribute } from "react";
 import uuid from "../../helpers/uuid"
 import Input from "../../components/Input";
 import Label from "../../components/Label";
+import SelectBox from "../SelectBox";
 
 interface Props {
-    inputRef: Ref<HTMLInputElement>;
+    inputRef: Ref<HTMLInputElement | HTMLSelectElement>;
     placeholder?: string;
     name: string;
-    type: HTMLInputTypeAttribute;
+    type: "select" | HTMLInputTypeAttribute;
+    options?: {
+        value: string;
+        label: string;
+    }[];
 }
 
-export default function Field({inputRef, name, placeholder, type} : Props) {
+export default function Field({inputRef, name, placeholder, type, options} : Props) {
     const id = uuid()
 
     return (
@@ -19,12 +24,21 @@ export default function Field({inputRef, name, placeholder, type} : Props) {
                 id={id}
                 name={name}
             />
-            <Input
-                inputRef={ inputRef }
-                type={type}
-                id={id}
-                placeholder={placeholder}
-            />
+            {
+                type === "select" ?
+                <SelectBox
+                    inputRef={ inputRef as Ref<HTMLSelectElement> }
+                    options={options || [] }
+                    placeholder={placeholder}
+                />
+                :
+                <Input
+                    inputRef={ inputRef as Ref<HTMLInputElement> }
+                    type={type}
+                    id={id}
+                    placeholder={placeholder}
+                />
+            }
         </div>
     )
 }
